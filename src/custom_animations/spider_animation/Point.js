cbd8bcd4fa0bd674273550f2f77afa423b9fded4
Napalys <napalys.klicius@gmail.com>
2023-12-08 12:58:26 +0100

    Removed distance calculation from spider class.

--- src/custom_animations/spider_animation/Point.js
+++ src/custom_animations/spider_animation/Point.js
@@ -45,11 +45,12 @@ export default class Point {
     }
   }
 
-  updateActiveStatus(targetDistance) {
+  updateActiveStatus(target) {
+    const targetDistance = Math.abs(getDistance(target, this));
     const thresholds = [
-      { limit: 4000, active: 0.3, circleActive: 0.6 },
-      { limit: 20000, active: 0.1, circleActive: 0.3 },
-      { limit: 40000, active: 0.02, circleActive: 0.1 },
+      { limit: 4000, active: 0.5, circleActive: 0.6 },
+      { limit: 20000, active: 0.3, circleActive: 0.3 },
+      { limit: 40000, active: 0.1, circleActive: 0.1 },
     ];
 
     // Find the first threshold that matches the target distance
