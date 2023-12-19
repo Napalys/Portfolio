c652319cc3ff53e83ef91c9dcec487175df72e33
Napalys <napalys.klicius@gmail.com>
2023-12-19 20:23:42 +0100

    Refactored projects to be loaded from json.

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,12 +1,12 @@
 import TechnologyIcon from './TechnologyIcon';
 
 export default class Project {
-  constructor(title, description, imageUrl, liveUrl, sourceUrl, icons) {
-    this.title = title;
-    this.description = description;
-    this.imageUrl = imageUrl;
-    this.liveUrl = liveUrl;
-    this.sourceUrl = sourceUrl;
-    this.icons = icons.map((icon) => new TechnologyIcon(icon.src, icon.label));
+  constructor(data) {
+    this.title = data.title;
+    this.description = data.description;
+    this.imageUrl = data.imageUrl;
+    this.liveUrl = data.liveUrl;
+    this.sourceUrl = data.sourceUrl;
+    this.icons = data.icons.map((icon) => new TechnologyIcon(icon.src, icon.label));
   }
 }
