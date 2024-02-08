e6571f40a1cd1d62180ca4c9d304ccd041180d8b
Napalys <napalys.klicius@gmail.com>
2024-02-08 19:57:36 +0100

    Added comets, which lightens up the atmosphere.

--- src/custom_animations/galactic_growth/galacticGrowth.js
+++ src/custom_animations/galactic_growth/galacticGrowth.js
@@ -2,7 +2,25 @@ import '../../style/components/_galacticGrowth.scss';
 
 const height = window.innerHeight * 3;
 const width = window.innerWidth;
-const body = document.body;
+const { body } = document;
+
+// Generation of comets
+((n) => {
+  const leftArr = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 60, 65, 70, 75, 80];
+  const delayArr = Array.from({ length: 25 }, (_, i) => (i + 1) * 2);
+
+  for (let i = 0; i < n; i += 1) {
+    const leftIndex = Math.floor(Math.random() * leftArr.length);
+    const left = leftArr[leftIndex] + parseFloat(Math.random().toFixed(2));
+    const delayIndex = Math.floor(Math.random() * delayArr.length);
+    const delay = delayArr[delayIndex] * 1000 + Math.round(Math.random() * 700);
+    const div = document.createElement('div');
+    div.className = 'star comet';
+    div.style.left = `${left}%`;
+    div.style.animationDelay = `${delay}ms`;
+    body.appendChild(div);
+  }
+})(25);
 
 // Generation of stars
 (function(n) {
